4998d7f29db97aac0a5505f4a1640d2c77263c99
nurik1347 <nurikmirzaev1347@gmail.com>
2024-03-19 21:39:35 +0500

    done

---
+++ main.js
@@ -0,0 +1,5 @@
+let userName = prompt('String typedagi malumotni kiriting')
+let userNumber = prompt('Number typedagi malumotni kiriting')
+let userBoolean = prompt('Boolean typedagi malumotni kiriting')
+alert(userName + userNumber + userBoolean)
+console.log(userName + userNumber + userBoolean);
